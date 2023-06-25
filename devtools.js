661f6c7e24ef5db8755a0e2475b3f0fcef6004b7
denialofself <basara3938@hotmail.com>
2023-06-25 15:31:32 -0400

    rollback filtering changes

--- devtools.js
+++ devtools.js
@@ -1,35 +1,29 @@
-function saveStringToFile(base64String, fileName) {
-    const decodedString = atob(base64String);
-    const blob = new Blob([decodedString], { type: 'text/plain' });
-    const url = URL.createObjectURL(blob);
-  
-    const anchor = document.createElement('a');
-    anchor.href = url;
-    anchor.download = fileName;
-  
-    // Programmatically click the link to trigger the download
-    anchor.click();
-  
-    // Clean up the URL object
-    URL.revokeObjectURL(url);
-  }
-  
-  browser.devtools.panels.create("Network Logger", "", "devtools-panel.html");
-  
+function saveStringToFile(content, fileName) {
+  const blob = new Blob([content], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
 
-  function handleRequestFinished(request) {
-    if (request.method === 'POST') {
-        console.log(request);
-        console.log("Server IP: ", request.serverIPAddress);
-        request.getContent().then(([content, mimeType]) => {
-        console.log("Content: ", content);
-        console.log("MIME type: ", mimeType);
-        if (mimeType === 'application/json') {
-            const newString = JSON.stringify(content);
-            saveStringToFile(newString, "D:\\temp\\LOGS\\output.txt");
-        }  
-    });
-  }
+  const anchor = document.createElement('a');
+  anchor.href = url;
+  anchor.download = fileName;
+
+  // Programmatically click the link to trigger the download
+  anchor.click();
+
+  // Clean up the URL object
+  URL.revokeObjectURL(url);
 }
   
-  browser.devtools.network.onRequestFinished.addListener(handleRequestFinished);
+browser.devtools.panels.create("Network Logger", "", "devtools-panel.html");
+  
+
+function handleRequestFinished(request) {
+    console.log(request);
+    console.log("Server IP: ", request.serverIPAddress);
+    request.getContent().then(([content, mimeType]) => {
+    console.log("Content: ", content);
+    console.log("MIME type: ", mimeType);
+    // saveStringToFile(content, "D:\\temp\\LOGS\\output.txt"); 
+    });    
+}
+  
+browser.devtools.network.onRequestFinished.addListener(handleRequestFinished);
